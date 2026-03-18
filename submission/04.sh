0f3f5e312e7cf6d39562b61772255bd0f30af7bb
Aaron <leebaaron@outlook.com>
2026-03-17 19:18:00 -0500

    Testing fund wallet

--- submission/04.sh
+++ submission/04.sh
@@ -1,4 +1,6 @@
 # Create a wallet with the name "builderswallet".
 bitcoin-cli -regtest -named createwallet wallet_name="builderswallet" descriptors=true
 
-bitcoin-cli -regtest -rpcwallet=builderswallet -generate 1
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+
+bitcoin-cli -regtest generatetoaddress 101 $ADDRESS
