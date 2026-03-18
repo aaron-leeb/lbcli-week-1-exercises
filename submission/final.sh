b8c5538cccb3ba0d0dea3aefe5f0cf7d2141090d
Aaron <leebaaron@outlook.com>
2026-03-17 19:51:51 -0500

    Fixed math in final.sh

--- submission/final.sh
+++ submission/final.sh
@@ -102,7 +102,7 @@ NEW_BALANCE=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getbalance)
 check_cmd "New balance check"
 echo "Your treasure balance: $NEW_BALANCE BTC"
 
-COLLECTED=$NEW_BALANCE - $BALANCE
+COLLECTED=$(echo "$NEW_BALANCE - $BALANCE" | bc -l)
 check_cmd "Balance calculation"
 echo "You've collected $COLLECTED BTC in treasures!"
 
