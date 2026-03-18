523993bd6f5103901279ca375aac5d455a5cf06a
Aaron <leebaaron@outlook.com>
2026-03-17 20:13:00 -0500

    Remove tr()

--- submission/final.sh
+++ submission/final.sh
@@ -176,7 +176,7 @@ check_cmd "Getting address info"
 
 # STUDENT TASK: Extract the internal key (the x-only pubkey) from the descriptor
 # WRITE YOUR SOLUTION BELOW:
-INTERNAL_KEY=$(echo "$ADDR_INFO" | jq -r '.desc' | sed -E 's/^tr\(([0-9a-fA-F]+)\).*$/\1/')
+INTERNAL_KEY=$(echo "$ADDR_INFO" | jq -r '.desc' | sed 's/^tr(\(.*\))#.*/\1/')
 check_cmd "Extracting key from descriptor"
 INTERNAL_KEY=$(trim "$INTERNAL_KEY")
 
