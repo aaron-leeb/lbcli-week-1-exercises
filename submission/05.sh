e083a7181e72bdef79ca77a60a93320434e6aefa
Aaron <leebaaron@outlook.com>
2026-03-17 19:11:40 -0500

    Added funds

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,2 @@
 # Check the total amount in the wallet.
-BALANCE=$(bitcoin-cli -regtest -rpcwallet=builderswallet getbalance)
-
-bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 1 "$BALANCE"
+bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
